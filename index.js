214675f5d71c7c493c8c7cc0529455a14ae78be0
NinaKWelch <ninakwelch@gmail.com>
2024-11-09 16:31:51 +0000

    exercise 3.2

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ app.get('/', (request, response) => {
   response.send('<h1>Phonebook API</h1>')
 })
 
+app.get('/info', (request, response) => {
+  response.send(
+    `<div>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+    </div>`
+  )
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
