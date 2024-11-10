a35f6319323ba0fcb9db9c679834d48b10d21b15
NinaKWelch <ninakwelch@gmail.com>
2024-11-10 19:17:37 +0000

    exercises 3.15 - 3.16

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.json())
-app.use(morgan('method :url :status :res[content-length] - :response-time ms :body'));
-app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
+app.use(morgan('method :url :status :res[content-length] - :response-time ms :body'));
 
 morgan.token('body', (request) => JSON.stringify(request.body))
 
@@ -32,17 +32,20 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      person ? response.json(person) : response.status(404).end()
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -65,6 +68,19 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware,
+// also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
